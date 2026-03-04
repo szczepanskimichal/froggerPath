6bab16e87da5c84e171156e587494e4c4c5f595e
michalszczepanski <michalszczepanski07@gmail.com>
2026-03-04 18:43:40 +0100

    I connected the buttons and I'll start writing the functions

---
+++ js/controller.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const prevBtn = document.querySelector('.controls button:nth-child(1)');
+  const nextBtn = document.querySelector('.controls button:nth-child(2)');
+  const newBoardBtn = document.querySelector('.controls button:nth-child(3)');
+});
